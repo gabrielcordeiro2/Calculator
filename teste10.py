18d15681f3e6f9f40248df405367cc7ab841cb85
gabrielcordeiro2 <gabriel.cordeiro.b.v@gmail.com>
2022-06-01 19:57:22 -0300

    atttttttt

---
+++ teste10.py
@@ -0,0 +1,20 @@
+numero = input()
+verificadores = ["0","1","2","3","4","+"]
+ite = 0
+max = len(numero)
+
+print(type(eval(numero)))
+
+
+
+
+
+
+
+
+
+# for i in numero:
+#     if i in verificadores:
+#         print("erro")
+#     else:
+#         print("ok")
