dd842e690be21aa8230020d52bd23478aaebfeb8
gabrielcordeiro2 <gabriel.cordeiro.b.v@gmail.com>
2022-06-02 01:21:06 -0300

    Organizing layout

--- calculadora_simplegui/interface.py
+++ calculadora_simplegui/interface.py
@@ -1,16 +1,14 @@
 # Import Module
+from turtle import left
 from PySimpleGUI import *
 
 class Interface:
     
     def imagem_calculadora():
-    
+		
         #GUI Layout
-        layout = [[Txt('' * 10)],
-    		[Input(default_text= '', size = (15, 1), font = ('Helvetica', 18),
-    				text_color = 'black', key = 'input')],
-    		[Txt('' * 10)],
-    		[ReadFormButton('c'), ReadFormButton('«')],
+        layout = [[Input(justification="left",default_text= '', size = (10, 3), font = ('Helvetica', 18), text_color = 'black', key = 'input'),ReadFormButton('«'),ReadFormButton('c')],
+    		[],
     		[ReadFormButton('7'), ReadFormButton('8'), ReadFormButton('9'), ReadFormButton('/')],
     		[ReadFormButton('4'), ReadFormButton('5'), ReadFormButton('6'), ReadFormButton('*')],
     		[ReadFormButton('1'), ReadFormButton('2'), ReadFormButton('3'), ReadFormButton('-')],
